84ee801d1500f0eca2928cc8494e1d96d5875aba
eduarda-pimentel <eduarda.pimentel001@gmail.com>
2025-01-07 10:33:03 -0300

    impedir hover de célula preenchida

--- src/components/tabuleiro.js
+++ src/components/tabuleiro.js
@@ -1,31 +1,49 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
-function Cell({colIndex, rowIndex, valoresCells, nrows}) {
+function Cell({colIndex, rowIndex, nrows, valoresCells, setValoresCells, turno, setTurno, atualizaTabuleiro}) {
     const isEven = ((colIndex+rowIndex)%2==0);
     const backgroundColour = isEven? '#F5F0CD' : '#B1F0F7';
     const [hover, setHover] = useState(false);
 
-    const handleMouseEnter = () => setHover(true);
+    const handleMouseEnter = () => {
+        if(turno==='user'){
+            setHover(true);
+        }
+    };
     const handleMouseLeave = () => setHover(false);
 
     const hoverBackground = hover ? (isEven?'#FADA7A':'#81BFDA') : backgroundColour;
 
-    const cellValue = valoresCells[rowIndex * nrows + colIndex];
+    const cellIndex = rowIndex*nrows + colIndex
+    const cellValue = valoresCells[cellIndex];
+
+    const handleClick = (e) =>{
+        e.preventDefault();
+        if (turno=='user'){
+            if (cellValue===''){
+                atualizaTabuleiro(cellIndex)
+                setTurno('computer')
+            } else {
+                alert('célula já preenchida, escolha outra')
+            }
+        }
+    }
 
     return (
-        <div
+        <button
             key={`${rowIndex}-${colIndex}`}
-            className="col shadow d-flex align-items-center justify-content-center"
+            className="col shadow d-flex align-items-center justify-content-center border-0"
             style={{ aspectRatio: '1', background: hoverBackground }}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
+            onClick={(e)=>handleClick(e)}
         >
             <p className='bg-transparent'>{cellValue}</p>
-        </div>
+        </button>
     );
 }
 
-function cellGenerator(valoresCells, turno) {
+function cellGenerator(valoresCells, setValoresCells, turno, setTurno, iconMatrix, atualizaTabuleiro) {
     const nrows = 3;
     const ncols = 3;
 
@@ -37,7 +55,12 @@ function cellGenerator(valoresCells, turno) {
                     colIndex={colIndex} 
                     rowIndex={rowIndex} 
                     valoresCells={valoresCells}
+                    setValoresCells={setValoresCells}
                     nrows={nrows}
+                    turno={turno}
+                    setTurno={setTurno}
+                    iconMatrix={iconMatrix}
+                    atualizaTabuleiro={atualizaTabuleiro}
                 />
             );
         })
@@ -46,11 +69,37 @@ function cellGenerator(valoresCells, turno) {
     return grid;
 }
 
-export function Tabuleiro(){
+export function Tabuleiro({iconMatrix}){
 
     const [valoresCells, setValoresCells] = useState(Array(9).fill(''));
     const [turno, setTurno] = useState('user');
-    const grid = cellGenerator(valoresCells, turno);
+    
+    
+    
+    const atualizaTabuleiro = (cellInd) =>{
+        const valoresCellsUpdate = [...valoresCells];
+        valoresCellsUpdate[cellInd] = iconMatrix[turno];
+        setValoresCells(valoresCellsUpdate);
+    }
+
+    const grid = cellGenerator(valoresCells, setValoresCells, turno, setTurno, iconMatrix, atualizaTabuleiro);
+
+    useEffect(() => {
+        const jogadaDoComputador = setTimeout(async()=> {
+            if (turno === 'computer') {
+                const indicesSortear = [];
+                valoresCells.forEach((value, index) => {
+                    if (value === '') {
+                        indicesSortear.push(index);
+                    }
+                });
+    
+                const randomCell = indicesSortear[Math.floor(Math.random() * indicesSortear.length)];
+                atualizaTabuleiro(randomCell);
+                setTurno('user'); 
+            } 
+        }, 2000)
+    }, [turno, valoresCells, atualizaTabuleiro]);
 
     return (
         <div className="container" style={{width:'450px', height:'450px'}}>
